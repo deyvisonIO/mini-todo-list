895e6a7814c4f4160b26078cfe7d3bf5dbbb84c9
Deyvison Iury <iurydeyvison@gmail.com>
2023-04-07 12:19:48 -0300

    Adding editing functionality

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
-import { removeTask } from '@/redux/tasksSlice';
+import { editTask, removeTask } from '@/redux/tasksSlice';
 import styles from '../styles/Home.module.css';
+import { useState } from 'react';
 
 interface propsInterface {
   task: string,
@@ -9,11 +10,46 @@ interface propsInterface {
 
 export function Task({ task, index }: propsInterface) {
   const dispatch = useDispatch();
+  const [newTask, setNewTask] = useState<string>(task);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  function changeEditingState(): undefined {
+    setIsEditing((previous:boolean) => !previous); 
+    return; 
+  }
+
+  function handleTaskSubmit(e: React.FormEvent<HTMLFormElement>): undefined {
+    const payload = {
+      index,
+      newTask
+    }
+    dispatch(editTask(payload));
+    changeEditingState();
+    return;
+  }
 
   return (
     <li>
-      <p>{task}</p>
-      <button className={styles.button} onClick={() => dispatch(removeTask({index}))}> X </button>
+      {isEditing ? 
+        <form onSubmit={handleTaskSubmit}>
+          <input 
+            value={newTask}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewTask(e.target.value)}
+          ></input>
+          <button className={styles.button}>Confirm</button>
+        </form> : 
+        <p>{task}</p>
+      }
+      {isEditing ? null :
+        <button 
+          className={styles.button}
+          onClick={() => dispatch(removeTask({index}))}
+        >
+          X 
+        </button>}
+      {isEditing ? null :
+        <button className={styles.button} onClick={changeEditingState}> EDIT </button>
+      }
     </li>
   );
 }
